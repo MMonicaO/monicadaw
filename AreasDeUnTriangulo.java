dd025f454526e3d32a0229dc78b4b4f73638c3b4
MMonicaO <148863976+MMonicaO@users.noreply.github.com>
2023-11-03 10:21:32 +0100

    Add files via upload

---
+++ AreasDeUnTriangulo.java
@@ -0,0 +1,17 @@
+
+public class AreasDeUnTriangulo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Hallar las areas de un triangulo
+		
+		int A= 
+		
+		
+		
+		
+		
+	}
+
+}
